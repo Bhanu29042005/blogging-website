ec8326a6ca4f8d59662c8d5ff3ff1c977623c556
Bhanu pratap singh <bhanu.tomtom@gmail.com>
2025-08-13 16:59:24 +0530

    Redesigned frontend

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,45 +1,16 @@
-import secure from "../assets/secure.jpg"
-
+import signupbg from "../assets/secure.jpg"
+// Quote.tsx
 export const Quote = () => {
+  
+};
+
+export const Quota = () => {
   return (
-    <div className="bg-slate-200 h-screen flex justify-center flex-col">
-      <div className="flex justify-center">
-        <div className="max-w-lg ">
-          <div className=" text-3xl font-bold">
-            "The customer support I received was exceptional. The support team
-            went above and beyond to address by concern"
-          </div>
-          <div className="max-w-md text-xl font-semibold text-left pt-4">
-            Shivam Kumar
-          </div>
-          <div className="max-w-md text-sm  text-slate-400">
-            CEO | Acme Inc
-          </div>
-
-        </div>
-
-      </div>
-      <div className="max-w-lg"></div>
+    <div className="h-screen flex items-center justify-center bg-cover bg-center "
+            style={{
+          backgroundImage: `url(${signupbg})`,
+        }}>
+      <Quote />
     </div>
   );
 };
-
-
-
-export const Quota=()=>{
-  return (
-    <div className="bg-slate- h-screen justify-center flex-col border-bla border-l">
-     
-      <div className=" pt-16 flex justify-center ">
-            <div className="max-w-2xl">
-              <img src={secure} alt="Secure" />
-              
-            </div>
-
-      </div>
-
-      <div className="max-w-lg"></div>
-
-    </div>
-  );
-}
